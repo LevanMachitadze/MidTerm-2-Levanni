44a06992d6e87f56a653f9d5924a2813e459653e
Levani <machitadzelevan@gmail.com>
2024-10-08 00:36:02 +0400

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const FetchData = React.memo(() => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://randomuser.me/api/?results=10'
+        );
+        setData(response.data.results);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <>
-      <h1 className="text-3xl font-bold text-center">Midterm 2</h1>
+      {data.map((user, index) => (
+        <div key={index}>
+          <div className='flex'>
+            <img src={user.picture.large} alt='img' />
+            <div>
+              <p>
+                {user.name.title}
+                {user.name.first}
+                {user.name.last}
+              </p>
+              <p>{`${user.age}/${user.gender}`}</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </>
   );
-}
+});
 
-export default App;
+export default FetchData;
